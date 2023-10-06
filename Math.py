494b5f83eb97b71539b52e0ad79e69fc4f2d3ef1
Jayanth <141720451+Jay16Nair@users.noreply.github.com>
2023-10-06 08:47:37 +0530

    Add files via upload

---
+++ Math.py
@@ -0,0 +1,15 @@
+import math
+pi=3.14
+a=-2
+print(round(pi))
+print(math.ceil(pi))
+print(math.floor(pi))
+print(abs(a))
+print(math.pow(a,3))
+print(math.sqrt(16))
+print(math.cbrt(125))
+x=5
+y=3
+z=7
+print(max(x,y,z))
+print(min(x,y,z))
