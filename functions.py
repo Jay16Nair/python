93b2e00aabda1271a6879d37f9515e27846989b8
Jayanth <141720451+Jay16Nair@users.noreply.github.com>
2023-10-06 08:46:32 +0530

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,20 @@
+
+def hello():
+    print("Hi!")
+
+hello()
+
+def hello(fname,lname):
+    print("Hi "+fname+" "+lname)
+hello("Jayanth","Nair")
+
+def add(a,b):
+    return a+b
+result=add(10,20)
+print("The addition of two numbes is: ",result) 
+
+def mul(a,b):
+    result=a*b
+    return result
+
+print("The product of two numbes is: ",mul(10,20)) 
