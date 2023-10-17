2d8f3448fb3934dab4e4ebfeb56b27d776097243
Jayanth <141720451+Jay16Nair@users.noreply.github.com>
2023-10-17 08:03:20 +0530

    Add files via upload

---
+++ area.py
@@ -0,0 +1,20 @@
+r=int(input("Enter radius"))
+area=3.14*r*r
+peri=2*3.14*r
+print("Area of circle is: ",area)
+print("Perimeter of circle is: ",peri)
+b=int(input("Enter breadth"))
+l=int(input("Enter length"))
+area1=l*b
+peri1=2*(l+b)
+print("Area of rectangle is: ",area1)
+print("Perimeter of rectangle is: ",peri1)
+ba=int(input("Enter base"))
+h=int(input("Enter height"))
+a1=int(input("Enter side1 "))
+a2=int(input("Enter side2 "))
+a3=int(input("Enter side3 "))
+area2=(ba*h)/2
+peri2=(a1+a2+a3)
+print("Area of traingle is: ",area2)
+print("Perimeter of triangle is: ",peri2)
